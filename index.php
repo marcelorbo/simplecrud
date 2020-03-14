732909f0afccc9fb9adc85da4bc4a9f18a4fea2a
mramosoliveira <mramos.oliveira@gmail.com>
2020-03-14 17:10:12 -0300

    commit

--- index.php
+++ index.php
@@ -1,12 +1,20 @@
 <?php
 
-require "config.php";
-require "vendor/autoload.php";
-
 use Core\App;
 use Core\Router;
 
 session_start();
+
+require_once("config.php");
+require_once("core/App.php");
+require_once("core/Router.php");
+require_once("core/Database.php");
+require_once("core/ActiveRecord.php");
+require_once("core/ViewBag.php");
+require_once("core/Controller.php");
+require_once("core/Table.php");
+
+require_once("vendor/autoload.php");
 
 $app = new App();
 $app->Start();
--- views/Master/Welcome.php
+++ views/Master/Welcome.php
@@ -16,19 +16,14 @@
   <!-- Favicon -->
   <link rel="icon" type="image/png" sizes="32x32" href="<?= CONFIG["BASEURL"]. "/assets/images/favicon-32x32.png" ?>">
 
-  <!-- CSS -->
-  <!-- Theme -->
-  <link rel="stylesheet" type="text/css" href="<?= CONFIG["BASEURL"]. "/assets/themes/sb-admin/css/sb-admin-2.css?version=". rand() ?>">
-
-  <!-- Vendor -->  
-  <link rel="stylesheet" type="text/css" href="<?= CONFIG["BASEURL"]. "/assets/themes/sb-admin/vendor/fontawesome-free/css/all.min.css" ?>" >
-  <link rel="stylesheet" type="text/css" href="<?= CONFIG["BASEURL"]. "/assets/themes/sb-admin/vendor/datatables/dataTables.bootstrap4.min.css" ?>">
+  <!-- Vendor CSS -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.20/css/dataTables.bootstrap4.min.css">
   <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.9/dist/css/bootstrap-select.min.css">
-  <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/gh/fancyapps/fancybox@3.5.7/dist/jquery.fancybox.min.css" />
   <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Fira+Sans:300,500" rel="stylesheet">
-  <link rel="stylesheet" type="text/css" href="<?= CONFIG["BASEURL"]. "/assets/css/quill.css?version=". rand() ?>">  
-  
-  <!-- Base -->
+
+  <!-- Base CSS -->
   <link rel="stylesheet" type="text/css" href="<?= CONFIG["BASEURL"]. "/assets/css/spinner.css?version=". rand() ?>">
   <link rel="stylesheet" type="text/css" href="<?= CONFIG["BASEURL"]. "/assets/css/snackbar.css?version=". rand() ?>">
   <link rel="stylesheet" type="text/css" href="<?= CONFIG["BASEURL"]. "/assets/css/style.css?version=". rand() ?>">  
@@ -52,33 +47,18 @@
   <!-- //. End View -->
 
   <!-- Vendor JS -->
-  <script src="<?= CONFIG["BASEURL"]. "/assets/themes/sb-admin/vendor/jquery/jquery.min.js" ?>"></script>
-  <script src="<?= CONFIG["BASEURL"]. "/assets/themes/sb-admin/vendor/bootstrap/js/bootstrap.bundle.min.js" ?>"></script>
-  <script src="<?= CONFIG["BASEURL"]. "/assets/themes/sb-admin/vendor/jquery-easing/jquery.easing.min.js" ?>"></script>
-  <script src="<?= CONFIG["BASEURL"]. "/assets/themes/sb-admin/vendor/datatables/jquery.dataTables.min.js" ?>"></script>
-  <script src="<?= CONFIG["BASEURL"]. "/assets/themes/sb-admin/vendor/datatables/dataTables.bootstrap4.min.js" ?>"></script>
+  <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+  <script src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js"></script>
+  <script src="https://cdn.datatables.net/1.10.20/js/dataTables.bootstrap4.min.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.9/dist/js/bootstrap-select.min.js"></script>
-  <script src="https://cdn.jsdelivr.net/gh/fancyapps/fancybox@3.5.7/dist/jquery.fancybox.min.js"></script>
-
-  <!-- Theme JS -->
-  <script src="<?= CONFIG["BASEURL"]. "/assets/themes/sb-admin/js/sb-admin-2.min.js?version=". rand() ?>"></script>
   
   <!-- Base JS -->
-  <!-- TODO: Review Order -->
-  <script src="<?= CONFIG["BASEURL"]. "/assets/js/moment.js" ?>"></script>
-  <script src="<?= CONFIG["BASEURL"]. "/assets/js/moment-business.js" ?>"></script>
-  <script src="<?= CONFIG["BASEURL"]. "/assets/js/moment-holiday.js" ?>"></script>
-  <script src="<?= CONFIG["BASEURL"]. "/assets/js/moment-pt-br.js" ?>"></script>
   <script src="<?= CONFIG["BASEURL"]. "/assets/js/validate.js" ?>"></script>
   <script src="<?= CONFIG["BASEURL"]. "/assets/js/mask.js" ?>"></script>
-  <script src="<?= CONFIG["BASEURL"]. "/assets/js/overlay.js" ?>"></script>
-  <script src="<?= CONFIG["BASEURL"]. "/assets/js/datepicker.js" ?>"></script>
-  <script src="<?= CONFIG["BASEURL"]. "/assets/js/datepicker.pt-BR.js" ?>"></script>
-  <script src="<?= CONFIG["BASEURL"]. "/assets/js/chart.bundle.min.js" ?>"></script>
-  <script src="<?= CONFIG["BASEURL"]. "/assets/js/quill.js" ?>"></script>  
-  <script src="<?= CONFIG["BASEURL"]. "/assets/js/quill-image-resize.js" ?>"></script>  
   <script src="<?= CONFIG["BASEURL"]. "/assets/js/app.js?version=". rand() ?>"></script>
-
+  
   <!-- Custom JS -->
   <script>
     $(document).ready(function() {
